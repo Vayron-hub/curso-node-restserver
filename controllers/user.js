0c558a765cb4ee39e762e29659e840bc10cec0c3
Vayron Conchas <vayron15conchas@gmail.com>
2024-03-26 13:46:00 -0600

    .env borrado

--- controllers/user.js
+++ controllers/user.js
@@ -1,47 +1,89 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+
+const Usuario = require('../models/usuario.js');
 
 
-const usuariosGet  = (req, res = response) => {
+const usuariosGet = async (req = request, res = response) => {
 
-    const {q,nombre = 'No Name',apkey}= req.query;
+    //const { q, nombre = 'No name', apkey } = req.query;
+
+    const { limite = 5, desde = 0 } = req.query;
+    const contarLosActivos = { estado: true };
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(contarLosActivos),
+        Usuario.find(contarLosActivos)
+            .skip(desde)
+            .limit(limite)
+    ]);
 
     res.json({
-        msg: 'get APi - controlador',
-        q,
-        nombre,
-        apkey
-    })
+       total,
+       usuarios
+    });
 }
 
-const usuariosPost  = (req, res = response) => {
+const usuariosPost = async (req, res = response) => {
 
-    const body = req.body;
+    try {
+        const { nombre, correo, password, rol } = req.body;
+        const usuario = new Usuario({ nombre, correo, password, rol });
 
-    res.json({
-        msg: 'post APi - controlador',
-        body
-    })
+        //Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        usuario.password = bcryptjs.hashSync(password, salt);
+
+        //Guardar en DB
+        await usuario.save();
+
+        res.json({
+            msg: 'post APi - controlador',
+            usuario
+        });
+    } catch (e) {
+        res.status(500).send({ mensaje: e });
+    }
 }
 
-const usuariosPut  = (req, res = response) => {
-    
+const usuariosPut = async (req, res = response) => {
+
     const { id } = req.params;
+    const { _id, password, google, correo, ...resto } = req.body;
 
-    res.json({
-        msg: 'put APi - controlador',
-        id
-    })
+    //Validar contra DB
+    if (password) {
+        //Encriptar la contraseña
+        const salt = bcryptjs.genSaltSync();
+        resto.password = bcryptjs.hashSync(password, salt);
+    }
+
+    const usuario = await Usuario.findByIdAndUpdate(id, resto);
+
+
+
+    res.json(usuario);
 }
 
-const usuariosPatch  = (req, res = response) => {
+const usuariosPatch = (req, res = response) => {
     res.json({
         msg: 'patch APi - controlador'
     })
 }
 
-const usuariosDelete  = (req, res = response) => {
+const usuariosDelete = async(req, res = response) => {
+
+    const { id } = req.params;
+
+    //FISICAMENTE LO BORRAMOS
+    // const usuario = await Usuario.findByIdAndDelete( id );
+
+    //CAMBIAR ESTADO DEL USUARIO
+    const usuario = await Usuario.findByIdAndUpdate( id, { estado: false} );
+
     res.json({
-        msg: 'delete APi - controlador'
+        id,
+        usuario
     })
 }
 
